9dde13f5d0bd595cf31003ab2bf714dac675334b
darinamilcheva <darrymilcheva@gmail.com>
2023-04-06 16:44:25 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,33 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+entry: {
+    
+  styles: './css/shape.scss'
+}
+
+
+$(document).ready(function() {
+
+  var html = '';
+  for (var i = 1; i <= 50; i ++) {
+      html += '<div class="shape-container--'+i+' shape-animation"><div class="random-shape"></div></div>';
+  }
+    
+  document.querySelector('.shape').innerHTML += html;
+  
+  var $allShapes = $("[class*='shape-container--']");
+  $('.button').click(function (event) {
+      $allShapes.toggleClass("stop-shape");
+      var $this = $(this);
+      $this.toggleClass('.button');
+      if($this.hasClass('.button')){
+          $this.text('Unfreeze shapes');         
+      } else {
+          $this.text('Freeze shapes');
+      }
+      event.preventDefault();
+  });
+  
+  });
